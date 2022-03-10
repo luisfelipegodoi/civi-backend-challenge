902e5a64afe27ce2d51ea2a71882a9271486cbf8
Luis Felipe <luis.godoi@picpay.com>
2022-03-10 08:22:56 -0300

    feat: added points handler for receive get request

---
+++ handlers/points.go
@@ -0,0 +1,75 @@
+package handlers
+
+import (
+	"civi-backend-challenge/models"
+	"civi-backend-challenge/services"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type Handler interface {
+	GetPoints(c *gin.Context)
+}
+
+type handler struct {
+	pointsService services.PointsService
+}
+
+func New(s services.PointsService) *handler {
+	return &handler{}
+}
+
+func (h *handler) GetPoints(c *gin.Context) {
+	queryParam := c.Request.URL.Query()
+
+	x, ok, err := isInteger(queryParam.Get("x"))
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.Error{
+			StatusCode: http.StatusBadRequest,
+			Message:    models.XParamInvalidOrNullError.Error(),
+			Details:    err.Error(),
+		})
+		return
+	}
+
+	y, ok, err := isInteger(queryParam.Get("y"))
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.Error{
+			StatusCode: http.StatusBadRequest,
+			Message:    models.YParamInvalidOrNullError.Error(),
+			Details:    err.Error(),
+		})
+		return
+	}
+
+	distance, ok, err := isInteger(queryParam.Get("distance"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, models.Error{
+			StatusCode: http.StatusBadRequest,
+			Message:    models.DistanceParamInvalidOrNullError.Error(),
+			Details:    err.Error(),
+		})
+		return
+	}
+
+	fmt.Println(x, y, distance)
+	c.JSON(http.StatusOK, "foi")
+}
+
+func isInteger(param string) (int, bool, error) {
+	var number int
+
+	if param == "" {
+		return number, false, errors.New("param is wrong, null or empty")
+	}
+
+	number, err := strconv.Atoi(param)
+	if err != nil {
+		return number, false, err
+	}
+
+	return number, true, nil
+}
