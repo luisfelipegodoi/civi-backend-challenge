e88fa78c5c453d029307f8f7f0d6d0ee2689b17e
Luis Felipe <luis.godoi@picpay.com>
2022-03-10 13:59:39 -0300

    feat: added main.go structure on project with calls

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"civi-backend-challenge/handlers"
+	"civi-backend-challenge/models"
 	"civi-backend-challenge/routers"
+	"civi-backend-challenge/services"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
+var (
+	port = os.Getenv("PORT")
+)
+
+func init() {
+	fmt.Println(fmt.Sprintf("initializing service on port:%s", port))
+}
+
 func main() {
-	endpoint := fmt.Sprintf(":%s", os.Getenv("PORT"))
-	initRouters := routers.InitRouter()
+	endpoint := fmt.Sprintf(":%s", port)
+
+	pointsFile, err := openFilePoints()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pointsService := services.NewPointService(pointsFile)
+	handlers := handlers.New(pointsService)
+
+	initRouters := routers.InitRouter(handlers)
 
 	server := &http.Server{
 		Addr:    endpoint,
@@ -17,4 +40,23 @@ func main() {
 	}
 
 	_ = server.ListenAndServe()
+}
+
+func openFilePoints() ([]*models.Points, error) {
+	filePath := os.Getenv("FILEPATH")
+	fileName := os.Getenv("FILENAME")
+	var points []*models.Points
+
+	pointsFile, err := os.Open(filePath + fileName)
+	if err != nil {
+		log.Printf("Error to open file with points values. The application can't initialize")
+		return nil, err
+	}
+
+	defer pointsFile.Close()
+
+	value, _ := ioutil.ReadAll(pointsFile)
+	json.Unmarshal(value, &points)
+
+	return points, nil
 }
