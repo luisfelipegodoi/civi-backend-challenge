f734e58c0401742c2b57e6fa835d2184969aced2
Luis Felipe <luis.godoi@picpay.com>
2022-03-10 14:02:13 -0300

    feat: added models on context

--- models/points.go
+++ models/points.go
@@ -5,3 +5,11 @@ type Points struct {
 	Y        int `json:"Y"`
 	Distance int `json:"Distance"`
 }
+
+type DistanceClassify []*Points
+
+func (d DistanceClassify) Len() int { return len(d) }
+
+func (d DistanceClassify) Less(i, j int) bool { return d[i].Distance < d[j].Distance }
+
+func (d DistanceClassify) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
