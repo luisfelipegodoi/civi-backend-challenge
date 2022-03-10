6cd6bfe747203bbe39c88571cd93d96a24748f5f
Luis Felipe <luis.godoi@picpay.com>
2022-03-10 08:26:58 -0300

    fix: fixed handler param in init router

--- routers/router.go
+++ routers/router.go
@@ -1,17 +1,18 @@
 package routers
 
 import (
+	"civi-backend-challenge/handlers"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"os"
 )
 
-func InitRouter() *gin.Engine {
+func InitRouter(h handlers.Handler) *gin.Engine {
 	router := gin.Default()
 
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("points")
+		v1.GET("points", h.GetPoints)
 	}
 
 	router.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
